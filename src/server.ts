4ae2ec4c1ca66f3c56a322cef62262b8ffa4a796
changmo <wlfah99@gmail.com>
2022-02-09 21:53:12 +0900

    migration

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,6 @@ const startServer = async () => {
     resolvers,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     introspection: true,
-
     context: async ({ req }) => {
       return {
         loggedInUser: await getUser(req.headers.authorization),
